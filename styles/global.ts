1ecb8be1554477f6130ed2de44a5e66e21a05787
Lauren Lin <urlun0404@gmail.com>
2022-11-05 01:32:58 +0800

    Initialize project

---
+++ styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body {
+        box-sizing: border-box;
+        width: 100%;
+
+    }
+    
+    ul {
+        list-style-type: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        border: 0;
+        background-color: transparent;
+    }
+
+    input, textarea {
+        &:focus {
+            outline: none;
+        }
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-size: 1rem;
+        font-weight: 400;
+        color: blue;
+    }
+`;
+
+export default GlobalStyles;
