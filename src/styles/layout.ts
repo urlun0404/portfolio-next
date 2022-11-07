2e4bae7a580089b6022147aca8b5cf0fbdf1c516
Lauren Lin <urlun0404@gmail.com>
2022-11-07 15:43:45 +0800

    Modify common layout

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,17 +2,21 @@ import styled from 'styled-components';
 import { theme } from 'styles/theme';
 
 export const Section = styled.section`
-  width: 100%;
-  padding: 1rem 0.75rem;
+  width: 100vw;
+  height: 100vh;
   margin: 2rem 0;
+  padding: 1rem 0.75rem;
   @media (max-width: ${theme.breakpoints.$sm}) {
     display: flex important!;
     flex-direction: column;
+    justify-content: flex-start;
     align-items: center;
+    height: auto;
   }
 `;
 
 export const SectionTitle = styled.h3`
+  padding: 0.5rem 0;
   @media (max-width: ${theme.breakpoints.$sm}) {
     text-align: center;
   }
